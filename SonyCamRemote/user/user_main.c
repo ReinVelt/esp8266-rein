25ed4aacae327cef3be5c0a0d7fa35e7ec6678fe
ReinVelt <rein@velt.org>
2017-01-09 13:43:43 +0100

    Update user_main.c
    updated comments

--- SonyCamRemote/user/user_main.c
+++ SonyCamRemote/user/user_main.c
@@ -1,27 +1,10 @@
 /*
  *  MQTT Switch
  *
- *  This firmware is meant to control a relay in e.g. a power outlet or power strip
- *  through MQTT.
+ *  This firmware is meant for remote control with the ESP8266 via WIFI of the Sony As/20 action camera
  *
- *  The ESP8266 will register itself with the MQTT server and will listen to topic
- *  /DeviceX/<chip-ID>. Inbound message are expected to be formatted as JSON messages
- *  and will be parsed for switching instruction. Please find a valid JSON instruction
- *  below:
  *
- *  {"switch":"off"}
- *
- *  The relay is supposed to be connected to ESP Pin GPIO2
- *  To experiment with the firmware, a LED will of course also do.
- *
- *  Optionally a push button can be connected meant to override messages from the
- *  MQTT broker, allowing you to physically switch the relay as well.
- *  When the push button is pressed, the relay will change its state and a JSON
- *  message is sent to the MQTT server indicating its new state.
- *  The optional push button should be connected to ESP Pin GPIO0 and when the
- *  button is pressed, this pin should be grounded.
- *
- *  (c) 2015 by Jan Penninkhof <jan@penninkhof.com>
+ *  (c) 2015 by Rein Velt (rein@mechanicape.com)
  *
  */
 #include "ets_sys.h"
